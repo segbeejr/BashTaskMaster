6e23cb40aa3f387f1b5a37162e6c5010534a1745
Tracy Sonyah <sonyahtracy@gmail.com>
2023-11-02 17:26:47 +0000

    Add tasks

---
+++ master.sh
@@ -0,0 +1,5 @@
+!#/binn/bash
+
+echo "What would you like to do? "
+echo
+echo -e "1. Read through a file\n2. Delete a file\n3. Change the name of a file\n4. Search through a file\n5. Display the last column in a file\n6. Display which grade can read the contents in that file\n7. Copies file to a remote server\n8. Generates random number every 5 seconds\nmore..."
